79db114a41574261127a99b8d3b2dad204d8a7c6
KonstantinKhan <khan366kos@gmail.com>
2022-09-14 12:25:13 +0500

    autoformatted

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import target from "../../resources/icons/target.png";
 import clients from "../../resources/icons/clients.png";
 import communicate from "../../resources/icons/communicate.png";
@@ -18,43 +18,43 @@ class Navigation extends Component {
                 <ul>
                     <li key={0}>
                         <Link className="menu__item" to="/target">
-                            <img draggable={false} src={target} alt="target"/>
+                            <img draggable={false} src={target} alt="target" />
                             <span>Цели</span>
                         </Link>
                     </li>
                     <li key={1}>
                         <Link className="menu__item" to="/clients">
-                            <img draggable={false} src={clients} alt="clients"/>
+                            <img draggable={false} src={clients} alt="clients" />
                             <span>Клиенты</span>
                         </Link>
                     </li>
                     <li key={2}>
                         <Link className="menu__item" to="/communicate">
-                            <img draggable={false} src={communicate} alt="communicate"/>
+                            <img draggable={false} src={communicate} alt="communicate" />
                             <span>Контакты</span>
                         </Link>
                     </li>
                     <li key={3}>
                         <Link className="menu__item" to="/handshake">
-                            <img draggable={false} src={handshake} alt="handshake"/>
+                            <img draggable={false} src={handshake} alt="handshake" />
                             <span>Сделки</span>
                         </Link>
                     </li>
                     <li key={4}>
                         <Link className="menu__item" to="/todo">
-                            <img draggable={false} src={toDoList} alt="todo"/>
+                            <img draggable={false} src={toDoList} alt="todo" />
                             <span>Задачи</span>
                         </Link>
                     </li>
                     <li key={5}>
                         <Link className="menu__item" to="/ability">
-                            <img draggable={false} src={ability} alt="ability"/>
+                            <img draggable={false} src={ability} alt="ability" />
                             <span>Возможности</span>
                         </Link>
                     </li>
                     <li key={6}>
                         <Link className="menu__item" to="/kpi">
-                            <img draggable={false} src={kpi} alt="KPI"/>
+                            <img draggable={false} src={kpi} alt="KPI" />
                             <span>KPI</span>
                         </Link>
                     </li>
