c683789d480c987b82702b6821bf4d38dad919a3
KonstantinKhan <khan366kos@gmail.com>
2022-10-12 08:34:51 +0500

    updated active links

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import target from "../../resources/icons/target.png";
 import clients from "../../resources/icons/clients.png";
 import communicate from "../../resources/icons/communicate.png";
@@ -18,52 +18,54 @@ class Navigation extends Component {
             <div className="navigation col-1">
                 <ul>
                     <li key={0}>
-                        <Link className="menu__item" to="/main">
+                        <NavLink 
+                            className="menu__item" to="/main">
                             <img draggable={false} src={main} alt="target" />
                             <span>Главная</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={1}>
-                        <Link className="menu__item" to="/target">
+                        <NavLink className="menu__item" to="/target">
                             <img draggable={false} src={target} alt="target" />
                             <span>Цели</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={2}>
-                        <Link className="menu__item" to="/clients">
+                        <NavLink className="menu__item" to="/clients">
                             <img draggable={false} src={clients} alt="clients" />
                             <span>Клиенты</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={3}>
-                        <Link className="menu__item" to="/communicate">
+                        <NavLink className="menu__item" to="/communicate">
                             <img draggable={false} src={communicate} alt="communicate" />
                             <span>Контакты</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={4}>
-                        <Link className="menu__item" to="/deals">
+                        <NavLink 
+                            className="menu__item" to="/deals">
                             <img draggable={false} src={handshake} alt="handshake" />
                             <span>Сделки</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={5}>
-                        <Link className="menu__item" to="/todo">
+                        <NavLink className="menu__item" to="/todo">
                             <img draggable={false} src={toDoList} alt="todo" />
                             <span>Задачи</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={6}>
-                        <Link className="menu__item" to="/ability">
+                        <NavLink className="menu__item" to="/ability">
                             <img draggable={false} src={ability} alt="ability" />
                             <span>Возможности</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li key={7}>
-                        <Link className="menu__item" to="/kpi">
+                        <NavLink className="menu__item" to="/kpi">
                             <img draggable={false} src={kpi} alt="KPI" />
                             <span>KPI</span>
-                        </Link>
+                        </NavLink>
                     </li>
                 </ul>
             </div>
